3dd2fd96dd529b8603ad3e49cc080505506d7778
SpiffyAF <couchstub@gmail.com>
2022-11-28 10:23:07 -0500

    Added more brands and templates

--- ICSP Template Creator/Template.cs
+++ ICSP Template Creator/Template.cs
@@ -21,17 +21,26 @@ namespace ICSP_Template_Creator
 
         public void initTemps()
         {
-            temps.Add(new Template(new List<string> { "SEER", "R410A", "FURNACE", "MINI SPLIT", "OIL FURNACE" }, "HVAC Equipment"));
+            temps.Add(new Template(new List<string> { "SEER", "R410A", "FURNACE", "MINI SPLIT", "OIL FURNACE", "RESI COIL", "PKG SYSTEM" }, "HVAC Equipment"));
             temps.Add(new Template(new List<string> { "MINI SPLIT WIRE", "OEM HVAC PARTS", "OIL FURNACE PARTS ACCY", "CONDENSATE DRAIN PANS", "ALLANSON TRANSFORMERS" }, "HVAC Supplies & Accessories"));
             temps.Add(new Template(new List<string> { "WATER COND" }, "Water Filters & Dispensers"));
             temps.Add(new Template(new List<string> { "AIR BRACE", "WMOB", "IMOB", "CULTURED MARBLE TOPS" }, "Starter Template"));
-            temps.Add(new Template(new List<string> { "PLUMBING SUPPLY AND STOP" }, "Plumbing Hardware"));
+            temps.Add(new Template(new List<string> { "PLUMBING SUPPLY AND STOP", "PLUMBING PRODUCTS" }, "Plumbing Hardware"));
             temps.Add(new Template(new List<string> { "ACME DRYER VENT" }, "Venting"));
             temps.Add(new Template(new List<string> { "ACME FURNACE PIPE" }, "Air Distribution"));
             temps.Add(new Template(new List<string> { "DRAIN PIPE" }, "Watering, Irrigation & Drainage"));
             temps.Add(new Template(new List<string> { "FLEX-TEX" }, "Clamping, Workholding & Positioning"));
             temps.Add(new Template(new List<string> { "PARS RUBBER GASKETS", "NON-ASBESTOS FLANGE" }, "Gaskets & Packing"));
             temps.Add(new Template(new List<string> { "BASINS" }, "Pumps"));
+            temps.Add(new Template(new List<string> { "KITCHEN AND BAR FAUCET", "LAVATORY FAUCET", "COMMERCIAL FAUCET", "FAUCET" }, "Faucets"));
+            temps.Add(new Template(new List<string> { "SINKS" }, "Sinks & Wash Fountains"));
+            temps.Add(new Template(new List<string> { "TUBS", "ACRYLIC BATH", "SHOWER DOORS", "VIKRELL SHOWER" }, "Tubs & Showers"));
+            temps.Add(new Template(new List<string> { "FIXTURE ACCY", "BATH ACCESSORIES", "BATHROOM ACCY" }, "Bathroom Hardware"));
+            temps.Add(new Template(new List<string> { "VALVES" }, "Valves & Actuators"));
+            temps.Add(new Template(new List<string> { "BOILER", "THERMAL EXPANSION" }, "Hydronics"));
+            temps.Add(new Template(new List<string> { "HEATER PARTS" }, "Water Heaters"));
+            temps.Add(new Template(new List<string> { "TOILET SEAT", "URINALS" }, "Toilets, Urinals & Parts"));
+            temps.Add(new Template(new List<string> { "DWV FITTING", "CPVC FITTING", "DWV FTG" }, "Pipe, Tubing & Hoses"));
 
         }
     }
